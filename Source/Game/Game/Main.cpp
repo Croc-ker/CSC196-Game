e60b0de5a23fb2a3881836e35c837ac76fce27a1
Croc-ker <123499932+Croc-ker@users.noreply.github.com>
2023-06-28 14:58:45 -0600

    Did some stuff

--- Source/Game/Game/Main.cpp
+++ Source/Game/Game/Main.cpp
@@ -1,13 +1,37 @@
 #include <iostream>
 #include "Core/Random.h"
 #include "Core/FileIO.h"
+#include "Core/Memory.h"
 using namespace std;
 
+
+
+void func()
+{
+	int* p = new int[1000000];
+}
 
 int main()
 {
 
+	/*for (int i = 0; i < 10; i++) {
+		int* p = new int;
+	}*/
+
+	/*while (true) {
+		func();
+	}*/
+
 	cout << kiko::getFilePath() << endl;
+	kiko::setFilePath("Assets");
+	cout << kiko::getFilePath() << endl;
+	size_t size;
+	kiko::getFileSize("game.txt", size);
+	cout << size << endl;
+
+	std::string s;
+	kiko::readFile("game.txt", s);
+	cout << s << endl;
 
 	kiko::seedRandom((unsigned int)time(nullptr));
 	for (int i = 0; i < 10; i++) {
